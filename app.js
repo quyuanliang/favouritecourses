3b07b63ccfa4df14b121d5b81eb2e48dc54769ad
CptJack <quyuanliang@gmail.com>
2017-05-31 04:25:04 -0400

    xxx

--- app.js
+++ app.js
@@ -71,7 +71,6 @@ configRoutes(app);
 
 // 监听端口，启动程序
 
-let port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log("1111111");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
